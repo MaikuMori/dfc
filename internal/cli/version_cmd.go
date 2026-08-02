5b781d1091143af8e6a1c3ccfca98221262bc318
Miks Kalnins <MiksKalnins@maikumori.com>
2026-08-02 18:57:26 +0200

    Fall back to build info for version output

--- internal/cli/version_cmd.go
+++ internal/cli/version_cmd.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"fmt"
 	"runtime"
+	"runtime/debug"
 )
 
 // Populated at build time via -ldflags by GoReleaser.
@@ -21,6 +22,33 @@ func (VersionCmd) Run() error {
 }
 
 func VersionString() string {
+	bi, _ := debug.ReadBuildInfo()
+	version, commit, date := resolveVersion(Version, Commit, Date, bi)
 	return fmt.Sprintf("dfc %s\ncommit: %s\ndate: %s\nruntime: %s %s/%s",
-		Version, Commit, Date, runtime.Version(), runtime.GOOS, runtime.GOARCH)
+		version, commit, date, runtime.Version(), runtime.GOOS, runtime.GOARCH)
+}
+
+// resolveVersion fills values not injected via ldflags from the embedded
+// build info: the module version for `go install` builds, the VCS
+// revision/time for source builds.
+func resolveVersion(version, commit, date string, bi *debug.BuildInfo) (string, string, string) {
+	if bi == nil {
+		return version, commit, date
+	}
+	if version == "dev" && bi.Main.Version != "" && bi.Main.Version != "(devel)" {
+		version = bi.Main.Version
+	}
+	for _, s := range bi.Settings {
+		switch s.Key {
+		case "vcs.revision":
+			if commit == "none" {
+				commit = s.Value
+			}
+		case "vcs.time":
+			if date == "unknown" {
+				date = s.Value
+			}
+		}
+	}
+	return version, commit, date
 }
